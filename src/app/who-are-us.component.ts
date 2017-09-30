a19747ae49a2e515afa5ba83ddba4d91a2c30bca
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2017-09-30 13:38:54 -0300

    who are us

--- src/app/who-are-us.component.ts
+++ src/app/who-are-us.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as data from "./who-are-us.data.json";
+
 @Component({
   selector: 'who-are-us',
   templateUrl: "./who-are-us.component.html"
 })
 export class WhoAreUsComponent implements OnInit {
-    ngOnInit() {
-    }
+  equip: any = data
+
+  ngOnInit() {
+  }
 }
